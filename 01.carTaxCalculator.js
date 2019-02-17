8072484ae6c86f98969c8327aeffc94f015d9593
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-02-17 11:10:55 +0200

    1st and 2nd exercises remade

--- 01.carTaxCalculator.js
+++ 01.carTaxCalculator.js
@@ -1,15 +1,21 @@
-function carTaxCalculator(power) {
-    power = Number(power);
+function solve(input) {
+    input = Number(input);
+    calculatePower(input);
 
-    if (power > 110) {
-        console.log(`${(1.54 * power).toFixed(2)} lv.`);
-    } else if (power > 74) {
-        console.log(`${(1.38 * power).toFixed(2)} lv.`);
-    } else if (power > 55) {
-        console.log(`${(0.68 * power).toFixed(2)} lv.`);
-    } else if (power > 37) {
-        console.log(`${(0.5 * power).toFixed(2)} lv.`);
-    } else {
-        console.log(`${(0.43 * power).toFixed(2)} lv.`);
+    function calculatePower(power) {
+        if (power > 110) {
+            console.log(`${(1.54 * power).toFixed(2)} lv.`);
+        } else if (power > 74) {
+            console.log(`${(1.38 * power).toFixed(2)} lv.`);
+        } else if (power > 55) {
+            console.log(`${(0.68 * power).toFixed(2)} lv.`);
+        } else if (power > 37) {
+            console.log(`${(0.5 * power).toFixed(2)} lv.`);
+        } else {
+            console.log(`${(0.43 * power).toFixed(2)} lv.`);
+        }
     }
 }
+
+
+solve(57.50);
--- 02.carTaxCalcularor2.js
+++ 02.carTaxCalcularor2.js
@@ -1,25 +1,31 @@
-function carTaxCalculator2(power, age) {
-    power = Number(power);
+function solve(input, age) {
+    input = Number(input);
     age = Number(age);
-    let coefficient = 0;
+    let sum = calculatePower(input) * calculateCoefficient(age);
 
-    if (age > 14) {
-        coefficient = 1;
-    } else if (age > 5) {
-        coefficient = 1.5;
-    } else {
-        coefficient = 2.8;
+    console.log(`${sum.toFixed(2)} lv.`);
+
+    function calculatePower(power) {
+        if (power > 110) {
+            return 1.54 * power;
+        } else if (power > 74) {
+            return 1.38 * power;
+        } else if (power > 55) {
+            return 0.68 * power;
+        } else if (power > 37) {
+            return 0.5 * power;
+        } else {
+            return 0.43 * power;
+        }
     }
 
-    if (power > 110) {
-        console.log(`${(1.54 * power * coefficient).toFixed(2)} lv.`);
-    } else if (power > 74) {
-        console.log(`${(1.38 * power * coefficient).toFixed(2)} lv.`);
-    } else if (power > 55) {
-        console.log(`${(0.68 * power * coefficient).toFixed(2)} lv.`);
-    } else if (power > 37) {
-        console.log(`${(0.5 * power * coefficient).toFixed(2)} lv.`);
-    } else {
-        console.log(`${(0.43 * power * coefficient).toFixed(2)} lv.`);
+    function calculateCoefficient(age) {
+        if (age > 14) {
+            return 1;
+        } else if (age > 5) {
+            return 1.5;
+        } else {
+            return 2.8;
+        }
     }
 }
